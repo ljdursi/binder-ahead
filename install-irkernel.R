85519413379a331ebe879d686edd587094d440c8
Jonathan Dursi <jonathan@dursi.ca>
2016-09-08 17:17:06 -0400

    Update install-irkernel.R

--- install-irkernel.R
+++ install-irkernel.R
@@ -1,3 +1,3 @@
-install.packages(c('crayon', 'pbdZMQ', 'devtools', 'Cairo'), repos=c('http://cran.utstat.utoronto.ca')
+install.packages(c('crayon', 'pbdZMQ', 'devtools', 'Cairo'), repos=c('http://cran.utstat.utoronto.ca'))
 devtools::install_github(paste0('IRkernel/', c('repr', 'IRdisplay', 'IRkernel')))
 IRkernel::installspec()
