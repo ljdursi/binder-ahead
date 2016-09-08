342be264e23b55ed8c709475acf9dcc9aa523ea5
Jonathan Dursi <jonathan@dursi.ca>
2016-09-08 19:23:56 -0400

    Delete install-irkernel.R

--- install-irkernel.R
+++
@@ -1,5 +0,0 @@
-install.packages(c('crayon', 'pbdZMQ', 'devtools'), repos=c('http://cran.utstat.utoronto.ca'))
-packageurl <- "https://cran.r-project.org/src/contrib/devtools_1.11.0.tar.gz"
-install.packages(packageurl, repos=NULL, type="source")
-devtools::install_github(paste0('IRkernel/', c('repr', 'IRdisplay', 'IRkernel')))
-IRkernel::installspec()
